370348ab0043edc11318d2fc5550e743a27e4df6
brynwibowo <42071051+brynwibowo@users.noreply.github.com>
2021-08-12 11:03:35 +0700

    update showme.php

--- showme.php
+++ showme.php
@@ -1,7 +1,7 @@
 <?php
 include "decode.php";
 
-$image = "sample.png"; //image yang akan diambil pesannya
+$image = "LSB-1bit_sample.png"; //image yang akan diambil pesannya
 
 $lets_decode = new decode();
 $lets_decode->printMsg($image);
